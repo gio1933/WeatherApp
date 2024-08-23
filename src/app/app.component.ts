48d8f5ac6dc9fbbe41af549ea6c62031ffb66ca8
desarrollador <desarrollorjg@outlook.com>
2024-08-23 15:59:32 -0600

    Fix Bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'WeatherApp';
+  weatherInfo: any = [];
+  appBackColor: string = "";
+  backImage: string = "";
+
+  contructor(private weather: WeatherService){
+    this.getCurrentPosition();
+    setTimeout(()=>{
+      if(this.weatherInfo.length === 0){
+        this.getWeatherInfo("mexico");
+      }
+      this.getStyle(this.weatherInfo);
+    },1000);
+  }
+
+  getCurrentPosition(){
+    this.weather.getLocation().then(resp => {
+      this.weather.getFromActualPosition(resp.lng, resp.lat).subscribe(data => {
+        this.weatherInfo = data;
+      });
+    });
+  }
+
+
+
 }
---
+++ src/app/weather.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  apiKey: string = "13dc7b5fa5931e769c3cba13b5aee73d";
+  uri: string = "";
+
+  constructor(private http: HttpClient) { }
+  getFromUserSelection(city:string){
+    this.uri = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
+    return this.http.get(this.uri);
+  }
+
+  getFromActualPosition(lon:string, lat:string){
+    this.uri = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+    return this.http.get(this.uri);
+  }
+
+  getLocation():Promise<any>{
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resp => {
+        resolve({lng:resp.coords.longitude, lat:resp.coords.latitude});
+      })
+    });
+  }
+}
